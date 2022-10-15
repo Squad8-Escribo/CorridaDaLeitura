5b4698b3d899857b7fa6367b28c0d05f7b7c07cb
Tiago Braga Fernandes <97136886+tiagobragafernandes@users.noreply.github.com>
2022-10-15 01:25:33 -0300

    play function improvments

--- public/js/index.js
+++ public/js/index.js
@@ -1,9 +1,14 @@
 const playBt = document.getElementsByClassName('game-play');
 const gameMenu = document.getElementById('game');
 const logo = document.getElementById('logo');
+const voltar = document.getElementById('back');
 
 function play() {
     gameMenu.style.display = "none";
     logo.style.display = "none";
-    location.href = '../../play.html'
+  let btn = setInterval(() => {
+        voltar.style.display = "flex";
+        voltar.onclick = () => document.location.reload();
+    }, 20);
 }
+
