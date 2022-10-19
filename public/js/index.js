9b81e87f92eeb63d7ffdb61bcfa3eba7f433fd76
Tiago Braga Fernandes <97136886+tiagobragafernandes@users.noreply.github.com>
2022-10-19 20:25:31 -0300

    Initial screen fixes, level screen and back button

--- public/js/index.js
+++ public/js/index.js
@@ -1,14 +1,28 @@
+const gameScreen = document.getElementById('bg')
 const playBt = document.getElementsByClassName('game-play');
 const gameMenu = document.getElementById('game');
 const logo = document.getElementById('logo');
-const voltar = document.getElementById('back');
+const levels = document.getElementById('game-levels');
+const fase = document.getElementsByClassName('fase');
 
 function play() {
     gameMenu.style.display = "none";
     logo.style.display = "none";
-  let btn = setInterval(() => {
-        voltar.style.display = "flex";
-        voltar.onclick = () => document.location.reload();
-    }, 20);
+    levelScreen()
+}
+
+function levelScreen(){
+    gameScreen.style.backgroundImage = "url('public/img/backgroundTelaDeFase.png')"
+    levels.style.display = "flex";
+
+    for(var i = 0; i<3; i++){
+        fase[i].onclick = primeiraFase; //TODO: Implementar array de fases
+    }
+    
+}
+
+function primeiraFase(){
+    alert("Primeira fase em breve...")
+    fase.window.reload();
 }
 
