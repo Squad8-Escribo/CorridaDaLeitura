165746f4b754ab65ebabba4cad13a0f4efa08880
matheuslimaandrade <matheusla2f04@gmail.com>
2022-10-20 22:00:50 -0300

    arquivo game.js

---
+++ public/js/game.js
@@ -0,0 +1,34 @@
+const car = document.getElementById('car');
+const sky = document.getElementById('sky');
+const road = document.getElementById('road');
+
+function skyAnimation() {
+    sky.classList.add('skyAnimation');
+}
+
+function roadAnimation() {
+    road.classList.add('roadAnimation')
+}
+
+function carAnimation() {
+    car.classList.add('carSuspension')
+  //car.classList.add('carJump');
+}
+// setTimeout(function() {
+//     car.classList.remove('carJump')
+// },300)
+
+
+document.addEventListener('keydown', function (event) {
+    skyAnimation();
+    roadAnimation();
+    carAnimation();
+})
+
+function carAnimation() {
+    car.classList.add('carSuspension')
+  //car.classList.add('carJump');
+}
+// setTimeout(function() {
+//     car.classList.remove('carJump')
+// },300)
