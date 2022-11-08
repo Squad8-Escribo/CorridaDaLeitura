962d85005d247cff4e3f2e25137808808e22f9bc
Matheus Lima Andrade <matheusla2f04@gmail.com>
2022-11-08 18:42:58 -0300

    changing functions

--- public/js/game.js
+++ public/js/game.js
@@ -1,23 +1,13 @@
 const car = document.getElementById('car');
-const road = document.getElementById('road');
-const background = document.getElementById('background');
+// const road = document.getElementById('road');
+const gameBackground = document.getElementById('background');
 
-function roadAnimation() {
-    road.classList.add('roadAnimation')
-    background.classList.add('roadAnimation')
+function raceAnimation() {
+    // road.classList.add('roadAnimation')
+    gameBackground.classList.add('roadAnimation')
 }
-
-function carAnimation() {
-    car.classList.add('carSuspension')
-  //car.classList.add('carJump');
-}
-// setTimeout(function() {
-//     car.classList.remove('carJump')
-// },300)
-
-
 function animationGame() {
-    roadAnimation();
+    raceAnimation()
     carAnimation();
 }
 
