8aed755989bc831ff7d3abc68e0caa99629931f1
Matheus Lima Andrade <matheusla2f04@gmail.com>
2022-11-08 18:42:48 -0300

    adding id's and creating functions

--- public/js/app.js
+++ public/js/app.js
@@ -7,6 +7,12 @@ const gameLevels = document.getElementById('game-levels');
 const level = document.getElementsByClassName('level');
 const gamePlay = document.getElementById('game-play');
 const wordbox = document.getElementById('word-box');
+const read = document.getElementById('read');
+const result = document.getElementById('result');
+const medal = document.getElementById('medal');
+const mic = document.getElementById('mic')
+const background = document.getElementById('background')
+const sky = document.getElementById('sky')
 
 function cleanPlay() {
     gamePlay.style.display = "none";
@@ -26,8 +32,31 @@ function playGame() {
     levelScreen();
 }
 
+// function backgroundOpacity () {
+//     mic.classList.add('mic-opacity')
+// }
+
+function backgroundOpacity() {
+    mic.classList.add('mic-opacity')
+    car.classList.add('car-opacity')
+    background.classList.add('background-opacity')
+    sky.classList.add('sky-opacity')
+}
+
 function gamePlayScreen() {
     gamePlay.style.display="flex";
+}
+
+function medalSelect(a) {
+    if (a==1) {
+        medal.innerHTML="  <img src='public/img/elements/diamond-medal.png'>"
+    } else if (a==2) {
+        medal.innerHTML="  <img src='public/img/elements/gold-medal.png'>"
+    } else if (a==3) {
+        medal.innerHTML="  <img src='public/img/elements/silver-medal.png'>"
+    } else if (a==4) {
+        medal.innerHTML="  <img src='public/img/elements/cooper-medal.png'>"
+    }
 }
 
 function levelScreen(){
@@ -41,8 +70,20 @@ function levelScreen(){
     }
 }
 
+// implementar função wordbox
 function wordBox() {
+    // if ----
     wordbox.style.display = "flex";
+
+    // if (a==1) {
+    //     wordbox.classList.add('word-box__word')
+    // } else if (a=='phrase') {
+    //     wordbox.classList.add('word-box__phrase')
+    // } else if (a='text') {
+    //     wordbox.classList.add('word-box__text')
+    // } else if (a==4) {
+    //     wordbox.classList.add('word-box__congrats')
+    // }
 }
 
 function showTutorial(){
